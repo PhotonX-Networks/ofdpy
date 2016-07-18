ae3e39a69ac2b7a5f0f1e50d3ccfc2e1e803889b
karel-van-de-plassche <karelvandeplassche@gmail.com>
2016-07-18 12:33:12 +0200

    Changed debugging

--- examples/ODL/dscp_one_way.py
+++ examples/ODL/dscp_one_way.py
@@ -14,18 +14,29 @@ from ofdpy import usecase
 from ofdpy import topology as topo
 
 # Configure logging levels
-logging.basicConfig(level=logging.DEBUG)
+# We only need to see warnings
+logging.basicConfig(level=logging.WARN)
 logger = logging.getLogger(__name__)
 logging.getLogger("dicttoxml").setLevel(logging.WARN)
 
-ofdpa_instance = ofdpa.OFDPA(mode="ODL", controller_ip="127.0.0.1")
+# But like some debug information from the messy ofdpy.odlparse
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARN)
+formatter = logging.Formatter('%(message)s')
+ch.setFormatter(formatter)
+logging.getLogger("ofdpy.odlparse").setLevel(logging.WARN)
+logging.getLogger("ofdpy.odlparse").propagate = False
+logging.getLogger("ofdpy.odlparse").addHandler(ch)
+
+ofdpa_instance = ofdpa.OFDPA(mode="ODL", controller_ip="131.155.34.9")
 
 # Configure topology. In this case, two servers with two NICs each, and one
 # OFDPA switch
 switch, nyx, ananke = topo.create_basic_tue_lab()
 
-# Start up the ping use case. After sending sending this to the ODL controller
-# with .ODL/send.py, the clients should be able to ping.
+# Start up the dscp ping use case. After sending sending this to the ODL controller
+# with .ODL/send.py, the clients should be able to send DHCP packets with tag
+# 60 to the target output port.
 usecase.dscp_vlan(ofdpa_instance, 10, 60,
               nyx.nics[0].connected_to.ofdpa_id,
              ananke.nics[0].connected_to.ofdpa_id)
